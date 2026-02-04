816cfb042a36fb6a3e1991b593f47f55cd093f68
0x0003 <0x0003@users.noreply.github.com>
2026-02-05 05:34:53 +0900

    Configure template

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "0x0003/0x0003-s-bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
